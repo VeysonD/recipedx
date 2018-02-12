47dd36812d1a05a0343d16e1c9eea57d2c6bf675
Rick Gallegos <duulketariakan@yahoo.com>
2018-02-11 21:35:49 -0800

    Finish configuring Auth login

--- src/app/services/auth/auth-config.ts
+++ src/app/services/auth/auth-config.ts
@@ -1,2 +1,15 @@
-export interface AuthConfig {
+interface AuthConfig {
+  CLIENT_ID: string;
+  CLIENT_DOMAIN: string;
+  AUDIENCE: string;
+  REDIRECT: string;
+  SCOPE: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  CLIENT_ID: 'vOVPX5dyYWbFmCkGb2ALyvt3O7xyyNUy',
+  CLIENT_DOMAIN: 'fdash4.auth0.com',
+  AUDIENCE: 'https://fdash4.auth0.com/userinfo',
+  REDIRECT: 'http://localhost:4200/callback',
+  SCOPE: 'openid profile email'
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,58 +1,74 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import 'rxjs/add/operator/filter';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import * as auth0 from 'auth0-js';
+
+import { AUTH_CONFIG } from './auth-config';
 
 @Injectable()
 export class AuthService {
 
   auth0 = new auth0.WebAuth({
-    clientID: 'vOVPX5dyYWbFmCkGb2ALyvt3O7xyyNUy',
-    domain: 'fdash4.auth0.com',
+    clientID: AUTH_CONFIG.CLIENT_ID,
+    domain: AUTH_CONFIG.CLIENT_DOMAIN,
     responseType: 'token id_token',
-    audience: `https://fdash4.auth0.com/userinfo`,
-    redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    audience: AUTH_CONFIG.AUDIENCE,
+    redirectUri: AUTH_CONFIG.REDIRECT,
+    scope: AUTH_CONFIG.SCOPE
   });
 
-  constructor(public router: Router) { }
+  userProfile: any;
+  loggedIn: boolean;
+  loggedIn$ = new BehaviorSubject<boolean>(this.loggedIn);
 
-  public handleAuthentication(): void {
-    this.auth0.parseHash((err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        window.location.hash = '';
-        this.setSession(authResult);
-        this.router.navigate(['/home']);
-      } else if (err) {
-        this.router.navigate(['/home']);
-        console.log(err);
-      }
-    });
+  constructor(public router: Router) {
+    if (this.authenticated) {
+      this.userProfile = JSON.parse(localStorage.getItem('profile'));
+      this.setLoggedIn(true);
+    } else {
+      this.logout();
+    }
   }
 
-  public isAuthenticated(): boolean {
-    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
-  }
-
-  public login(): void {
+  login(): void {
     this.auth0.authorize();
   }
 
-  public logout(): void {
-    localStorage.removeItem('access_token');
+  logout(): void{
+    localStorage.removeItem('token');
     localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
     localStorage.removeItem('expires_at');
-
-    this.router.navigate(['/home']);
+    this.userProfile = undefined;
+    this.setLoggedIn(false);
   }
 
-  private setSession(authResult): void {
-    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+  setLoggedIn(value: boolean): void {
+    this.loggedIn$.next(value);
+    this.loggedIn = value;
+  }
 
-    localStorage.setItem('access_token', authResult.accessToken);
+  private _getProfile(authResult): void {
+    this.auth0.client.userInfo(authResult.accessToken, (err, profile) => {
+      this._setSession(authResult, profile);
+    })
+  }
+
+  private _setSession(authResult, profile): void {
+    const expTime = authResult.exipresIn * 1000 + Date.now();
+
+    localStorage.setItem('token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('expires_at', expiresAt);
+    localStorage.setItem('profile', JSON.stringify(profile));
+    localStorage.setItem('expires_at', JSON.stringify(expTime));
+    this.userProfile = profile;
+    this.setLoggedIn(true);
+  }
+
+  get authenticated(): boolean {
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return Date.now() < expiresAt;
   }
 
 }
