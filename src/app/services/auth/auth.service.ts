3ad0b83d292b126209d5d3bf3eb0bf728822b1c7
Rick Gallegos <duulketariakan@yahoo.com>
2018-02-11 19:18:16 -0800

    write auth0 service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,8 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import * as auth0 from 'auth0-js';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  auth0 = new auth0.WebAuth({
+    clientID: process.env.CLIENT_ID,
+    domain: process.env.DOMAIN,
+    responseType: 'token id_token',
+    audience: `https://${process.env.DOMAIN}/userinfo`,
+    redirectUri: process.env.AUTH_REDIRECT,
+    scope: 'openid'
+  });
+
+  constructor(public router: Router) { }
+
+  public handleAuthentication(): void {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = '';
+        this.setSession(authResult);
+        this.router.navigate(['/home']);
+      } else if (err) {
+        this.router.navigate(['/home']);
+        console.log(err);
+      }
+    });
+  }
+
+  public isAuthenticated(): boolean {
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+  public login(): void {
+    this.auth0.authorize();
+  }
+
+  public logout(): void {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+
+    this.router.navigate(['/']);
+  }
+
+  private setSession(authResult): void {
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
 
 }
