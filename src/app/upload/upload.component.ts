60d31906e8873c51479d17455937877f026fa718
Rick Gallegos <duulketariakan@yahoo.com>
2018-02-21 18:21:29 -0800

    Add two-way binding and a test model for ngForm

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -10,7 +10,9 @@ export class UploadComponent implements OnInit {
   recipe: Upload;
   submitted: boolean;
 
+  //TODO: delete dTags and model
   dTags = ['cheese', 'pizza', 'pepperoni', 'olives', 'anchovies'];
+  model = new Upload('Bob', 'Curry', ['picture1', 'picture2'], ['spice', 'hot', 'rice']);
 
   constructor() { }
 
