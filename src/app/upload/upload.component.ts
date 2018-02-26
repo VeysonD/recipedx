d9ac803aca1175ae746c7d36ab72c4b5da3a0558
Rick Gallegos <duulketariakan@yahoo.com>
2018-02-25 17:24:46 -0800

    Add and delete tag functionality

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -12,6 +12,7 @@ export class UploadComponent implements OnInit {
   //TODO: Clean up variables that are not used
   recipe: Upload;
   submitted: boolean;
+  tagInput: string;
 
   //TODO: Delete dTags
   dTags = ['cheese', 'pizza', 'pepperoni', 'olives', 'anchovies'];
@@ -20,8 +21,8 @@ export class UploadComponent implements OnInit {
 
   ngOnInit() {
     const user = JSON.parse(localStorage.getItem('profile')).email;
-    this.recipe = new Upload(user, '', '', false, []);
-
+    this.recipe = new Upload(user, '', null, false, []);
+    this.tagInput = '';
     this.submitted = false;
   }
 
@@ -29,8 +30,18 @@ export class UploadComponent implements OnInit {
     this.recipe.Photos = files;
   }
 
-  handleTagInput(tag: string): void {
+  handleTagDelete(index: number): void {
+    console.log('what is the index of the tag: ', index);
+    this.recipe.Tags.splice(index, 1);
+  }
 
+  handleTagInput(): void {
+    //TODO: Handle if there are no Tags (need at least one and up to 12)
+    console.log('handle tag input');
+    if (this.tagInput.length !== 0) {
+      this.recipe.Tags.push(this.tagInput);
+      this.tagInput = '';
+    }
   }
 
   onSubmit() {
@@ -43,5 +54,10 @@ export class UploadComponent implements OnInit {
       }, error => {
         console.error('Upload recipe error: ', error);
       });
+    // Reset the upload recipe
+    this.recipe.title = '';
+    this.recipe.Photos = null;
+    this.recipe.isStarred = false;
+    this.recipe.Tags = [];
   }
 }
