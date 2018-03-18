054032d7e0e4a6fc70eb2de6ff6405d7016b9cbd
Rick Gallegos <duulketariakan@yahoo.com>
2018-03-18 15:31:48 -0700

    Add modal code to tourist component

--- src/app/tourist/tourist.component.ts
+++ src/app/tourist/tourist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 import { AuthService } from '../services/auth/auth.service';
 
 @Component({
@@ -7,11 +9,33 @@ import { AuthService } from '../services/auth/auth.service';
   styleUrls: ['./tourist.component.css']
 })
 export class TouristComponent implements OnInit {
+  closeResult: string;
 
-  constructor(public authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit() {
     this.authService.handleAuthenticated();
+  }
+
+  open(content): void {
+    this.modalService.open(content).result.then(result => {
+      this.closeResult = `Closed with: ${result}`;
+    }, reason => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
